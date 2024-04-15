d112109d50836d0b628aab00c7a1c6c0945b082f
kof99athena <kof99athena@gmail.com>
2024-04-15 22:57:47 +0900

    [Interface] 오버라이드 메소드 금지

--- app/src/main/java/com/anehta/athenakotlinlab/KotlinLab.kt
+++ app/src/main/java/com/anehta/athenakotlinlab/KotlinLab.kt
@@ -19,5 +19,5 @@ class RedButton : Clickable, Focusable {
 open class RichButton : Clickable {
     fun disable() {} //이 함수는 파이널이다. 하위클래스가 이 메소드를 오버라이드 할 수 없다.
     open fun animate() {} //이 함수는 열려있다. 하위클래스에서 이 메소드를 오버라이드 할 수 있다.
-    override fun click(){} // 이 함수는 (상위클래스에서 선언된) 열려있는 메소드를 오버라이드한다. 오버라이드 한 함수는 기본적으로 열려있다.
+    final override fun click(){} // 이 함수는 (상위클래스에서 선언된) 열려있는 메소드를 오버라이드한다. 오버라이드 한 함수는 기본적으로 열려있다.
 }
