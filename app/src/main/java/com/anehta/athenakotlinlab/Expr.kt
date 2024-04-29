9df5028d669794cac5b91a85d467aaa40b32aff1
kof99athena <kof99athena@gmail.com>
2024-04-30 01:18:23 +0900

    [sealed class] sealed 변경자를 이용하여 when식 이용하기

--- app/src/main/java/com/anehta/athenakotlinlab/Expr.kt
+++ app/src/main/java/com/anehta/athenakotlinlab/Expr.kt
@@ -4,8 +4,28 @@ interface Expr
 class Num(val value: Int) : Expr
 class Sum(val left: Expr, val right: Expr) : Expr
 
-//fun eval(e: Expr): Int {
-//    if (e is Num) {
+sealed class Expr2 {
+    class Num(val value: Int) : Expr2()
+    //class Num2(val value: Int) : Expr()
+    class Sum(val left: Expr, val right: Expr) : Expr2()
+}
+
+fun eval2(e: Expr2): Int =
+    when (e) {
+        is Expr2.Num -> e.value
+        is Expr2.Sum -> eval(e.left) + eval(e.right)
+    }
+
+fun eval(e: Expr): Int =
+    when (e) {
+
+        is Num -> e.value
+        is Sum -> eval(e.left) + eval(e.right)
+        else ->
+            throw IllegalArgumentException("Unknown Expression")
+
+
+        //    if (e is Num) {
 //        val n = e as Num
 //        return n.value
 //    }
@@ -13,7 +33,8 @@ class Sum(val left: Expr, val right: Expr) : Expr
 //        return eval(e.right) + eval(e.left)
 //    }
 //    throw IllegalArgumentException("Unknown Expression")
-//}
+    }
+
 
 fun main() {
     println(evalWithLogging(Sum(Sum(Num(1), Num(2)), Num(4))))
