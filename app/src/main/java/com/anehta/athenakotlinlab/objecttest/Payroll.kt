3065a976736b1a035bfeb993a814fd40c933d1ea
kof99athena <kof99athena@gmail.com>
2024-05-20 00:20:22 +0900

    [object] 클래스안에 객체 선언하기

--- app/src/main/java/com/anehta/athenakotlinlab/objecttest/Payroll.kt
+++ app/src/main/java/com/anehta/athenakotlinlab/objecttest/Payroll.kt
@@ -4,7 +4,19 @@ import com.anehta.athenakotlinlab.Person
 import java.io.File
 
 fun main() {
-    println(CaseInsensitiveFileComparator.compare(File("/User"),File("/User")))
+    println(CaseInsensitiveFileComparator.compare(File("/User"), File("/User")))
+
+    val person = listOf(com.anehta.athenakotlinlab.objecttest.Person("kim"),com.anehta.athenakotlinlab.objecttest.Person("lee"))
+    println(person.sortedWith(com.anehta.athenakotlinlab.objecttest.Person.NameComparator))
+}
+
+data class Person(val name: String) {
+    object NameComparator : Comparator<com.anehta.athenakotlinlab.objecttest.Person> {
+        override fun compare(
+            o1: com.anehta.athenakotlinlab.objecttest.Person,
+            o2: com.anehta.athenakotlinlab.objecttest.Person
+        ): Int = o1.name.compareTo(o2.name)
+    }
 }
 
 object CaseInsensitiveFileComparator : Comparator<File> {
