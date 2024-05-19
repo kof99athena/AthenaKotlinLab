d694d6d8de90a4986abfd6bdec74d81a517e90ce
kof99athena <kof99athena@gmail.com>
2024-05-19 19:09:56 +0900

    [data class] hashcode 일치시키기

--- app/src/main/java/com/anehta/athenakotlinlab/dataclasstest/Client.kt
+++ app/src/main/java/com/anehta/athenakotlinlab/dataclasstest/Client.kt
@@ -4,9 +4,14 @@ fun main() {
     val client1 = Client("고양이", 1111)
     val client2 = Client("고양이", 1111)
     println(client1 == client2) //true
+
+    val processed = hashSetOf(Client("고양이", 1111))
+    println(processed.contains(Client("고양이", 1111))) //true
 }
 
 class Client(val name: String, val postalCode: Int) {
+    override fun hashCode(): Int = name.hashCode() * 31 + postalCode
+
     override fun equals(other: Any?): Boolean {
         if (other == null || other !is Client)
         //null이거나 Client 객체가 아니라면?
