2cac21837ff45ec021e7bc647150752411e51759
kof99athena <kof99athena@gmail.com>
2024-05-19 18:54:33 +0900

    [data class] equals 오버라이드

--- app/src/main/java/com/anehta/athenakotlinlab/dataclasstest/Client.kt
+++ app/src/main/java/com/anehta/athenakotlinlab/dataclasstest/Client.kt
@@ -1,10 +1,20 @@
 package com.anehta.athenakotlinlab.dataclasstest
 
 fun main() {
-    println(Client("kim",1111).toString())
+    val client1 = Client("고양이", 1111)
+    val client2 = Client("고양이", 1111)
+    println(client1 == client2) //true
 }
 
 class Client(val name: String, val postalCode: Int) {
+    override fun equals(other: Any?): Boolean {
+        if (other == null || other !is Client)
+        //null이거나 Client 객체가 아니라면?
+            return false
+        return name == other.name &&
+                postalCode == other.postalCode
+    }
+
     override fun toString() = "Client(name=$name, postalCode=$postalCode)"
     //결과값
     //override 시: Client(name=kim, postalCode=1111)
